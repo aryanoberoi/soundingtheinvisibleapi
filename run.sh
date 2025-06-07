2cff3716e95bb1560c91ea5035653c365aebf945
khoparzi <abhinayk@gmail.com>
2025-06-07 16:39:58 +0530

    Fixing the run script

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ tmux -2 attach-session -t sti || tmux -2 \
   split-window -v -t sti \; \
   send-keys -t 0 "cd ~/Dev/soundingtheinvisibleapi" C-m \; \
   send-keys -t 0 "sclang soundserver.scd" C-m \; \
+  send-keys -t 1 "cd ~/Dev/soundingtheinvisibleapi" C-m \; \
   send-keys -t 1 "source invisible-sound/bin/activate" C-m \; \
   send-keys -t 1 "python main_server.py" C-m \; \
   select-pane -t 0
