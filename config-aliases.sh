f5c3e5db0a8f20e1db7feeb405ccda2ea832f97f
khoparzi <abhinayk@gmail.com>
2025-06-07 19:52:35 +0530

    Added update-api function and attach-server alias

--- config-aliases.sh
+++ config-aliases.sh
@@ -2,8 +2,14 @@ function change-tank() {
   sudo raspi-config nonint do_hostname invisible-sound-$@.local;
 }
 
+function update-api() {
+  cd ~/Dev/soundingtheinvisibleapi
+  git pull
+}
+
 export JACK_NO_AUDIO_RESERVATION=1
 export QT_QPA_PLATFORM=offscreen
 export DISPLAY=:0
 alias invisible-sound="~/Dev/soundingtheinvisibleapi/run.sh"
 alias change-wifi="sudo nano /boot/firmware/wpa_supplicant.conf"
+alias attach-server="tmux attach -t sti"
