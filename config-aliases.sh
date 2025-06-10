d6a01885e96e16b4a730be76ffbacf7a9bcdd8f0
khoparzi <abhinayk@gmail.com>
2025-06-11 01:03:16 +0530

    Update config-aliases.sh to have a edit-wifi to change wpa_supplicant
    Change wifi now uses raspi-config
    Check wifi checks wifi connection

--- config-aliases.sh
+++ config-aliases.sh
@@ -2,6 +2,10 @@ function change-tank() {
   sudo raspi-config nonint do_hostname invisible-sound-$@.local;
 }
 
+function change-wifi() {
+  sudo raspi-config nonint do_wifi_ssid_passphrase $1 '$2'
+}
+
 function update-api() {
   cd ~/Dev/soundingtheinvisibleapi
   git pull
@@ -11,5 +15,6 @@ export JACK_NO_AUDIO_RESERVATION=1
 export QT_QPA_PLATFORM=offscreen
 export DISPLAY=:0
 alias invisible-sound="~/Dev/soundingtheinvisibleapi/run.sh"
-alias change-wifi="sudo nano /boot/firmware/wpa_supplicant.conf"
+alias edit-wifi="sudo nano /boot/firmware/wpa_supplicant.conf"
+alias check-wifi="iwconfig"
 alias attach-server="tmux attach -t sti"
